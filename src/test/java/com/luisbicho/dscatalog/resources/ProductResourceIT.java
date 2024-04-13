4aa7ea4e88d59be2a6e87c973e707a833b91c353
luiscbicho <159563337+luiscbicho@users.noreply.github.com>
2024-04-13 22:29:00 +0200

    Integration Tests - ProductService[findAllPaged,Delete], ProductResource[findAll,Update]

---
+++ src/test/java/com/luisbicho/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,83 @@
+package com.luisbicho.dscatalog.resources;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luisbicho.dscatalog.dto.ProductWithCategoryDTO;
+import com.luisbicho.dscatalog.tests.Factory;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+
+    }
+
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/products?page=0&size=12&sort=name,asc").accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(jsonPath("$.content").exists());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+        ProductWithCategoryDTO dto = Factory.createProductWithCategoryDTO();
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        String expectedName = dto.getName();
+        String expectedDescription = dto.getDescription();
+
+        ResultActions result = mockMvc.perform(put("/products/{id}", existingId).content(jsonBody).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingId));
+        result.andExpect(jsonPath("$.name").value(expectedName));
+        result.andExpect(jsonPath("$.description").value(expectedDescription));
+
+    }
+
+    @Test
+    public void updateShouldReturnThrowResourceNotFoundExceptionWhenIdDoesNotExist() throws Exception {
+        ProductWithCategoryDTO dto = Factory.createProductWithCategoryDTO();
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/products/{id}", nonExistingId).content(jsonBody).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNotFound());
+    }
+
+
+}
---
+++ src/test/java/com/luisbicho/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,69 @@
+package com.luisbicho.dscatalog.services;
+
+import com.luisbicho.dscatalog.dto.ProductWithCategoryDTO;
+import com.luisbicho.dscatalog.repositories.ProductRepository;
+import com.luisbicho.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+@SpringBootTest
+@Transactional
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService service;
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteProductWhenIdExists() throws Exception {
+        service.delete(existingId);
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() throws Exception {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> service.delete(nonExistingId));
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPageOfProducts() throws Exception {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Page<ProductWithCategoryDTO> result = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnSorteredPageOfProductsWhenSortByName() throws Exception {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+        Page<ProductWithCategoryDTO> result = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+    }
+}
